b29e9a48f59f7cf09226491e4c630fc390d57c2e
barry.ye <barry.ye@vipshop.com>
2014-09-23 17:53:19 +0800

    collect tcp packet

---
+++ sniffer.py
@@ -0,0 +1,93 @@
+# coding: utf8
+import socket
+import struct
+import sys
+import datetime
+sys.path.append('./lib/')
+import pymongo
+from bson.binary import Binary
+# https://docs.python.org/2/library/socket.html
+# try:
+#     from http_parser.parser import HttpParser
+# except ImportError:
+#     from http_parser.pyparser import HttpParser
+# 如果获取所有ip层数据包
+s = socket.socket( socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
+# 似乎如果是仅tcp协议，抓不到包。有空研究
+# s = socket.socket( socket.AF_PACKET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+# 这种模式不会给eth_header
+# s = socket.socket( socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+host_name = socket.gethostname()
+local_ip = socket.gethostbyname(host_name)
+# p = HttpParser()
+mongo_db = pymongo.MongoClient('mongodb://10.101.130.213/sniffer').sniffer
+while True:
+    packet, address = s.recvfrom(65535)
+    #if (packet.find('HTTP') == i-1):
+    #    continue
+    eth_header_len = 14
+    eth_header = struct.unpack('!6s6sH', packet[:eth_header_len])
+    eth_protocol = socket.ntohs(eth_header[2])
+    # dest_mac = packet[:6]
+    # src_mac = packet[6:12]
+
+    # IP packets
+    if eth_protocol != 8:
+	continue
+    # 20
+    ip_header = struct.unpack('!BBHHHBBH4s4s', packet[eth_header_len:eth_header_len + 20])
+    version = ip_header[0] >> 4
+    ip_header_len = (ip_header[0] & 0xF) * 4
+    ttl = ip_header[5]
+    ip_protocol = ip_header[6]
+    src_ip = socket.inet_ntoa(ip_header[8])
+    dest_ip = socket.inet_ntoa(ip_header[9])
+
+    # TCP protocol
+    if ip_protocol != 6:
+        continue
+    tcp_start = eth_header_len + ip_header_len
+    tcp_header = struct.unpack('!HHLLBBHHH', packet[tcp_start:tcp_start + 20])
+    src_port = tcp_header[0]
+    dest_port = tcp_header[1]
+    # todo: 重组http协议
+    #　对应请求的　acknowledgement
+    sequence = tcp_header[2]
+    # 重组使用
+    acknowledgement = tcp_header[3]
+    doff_reserved = tcp_header[4]
+    tcp_header_len = doff_reserved >> 4
+    headers_size = eth_header_len + ip_header_len + tcp_header_len*4
+    tcp_body = packet[headers_size:]
+
+    # filter
+    if not (src_port == 80 or dest_port == 80):
+        continue
+    try:
+        tcp_body = str(tcp_body)
+        data = {
+            "raw_packet": Binary(packet),
+            "tcp_packet_info": {
+                "body": tcp_body,
+                "sequence": sequence,
+                "acknowledgement": acknowledgement,
+                "doff_reserved": doff_reserved
+            },
+            "time": datetime.datetime.now(),
+            "src": "%s:%s" % (src_ip, src_port),
+            "dest": "%s:%s" % (dest_ip, dest_port),
+            "host_ip": local_ip,
+            "host_name": host_name
+        }
+        mongo_db.packet.save(data)
+    except:
+        continue
+    # print 'got packet'
+    # try:
+    #     p.execute(packet, len(packet))
+    #     print 'success'
+    #     print p.recv_body()
+    #     print p.get_headers()
+    # except Exception as e:
+    #     print e
+    #     pass
